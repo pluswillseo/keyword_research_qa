bcd49c7f2bcf0cdc90cdda36da7cee9e90dda3bf
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-16 12:01:29 +0000

    Update app-st.py

--- app-st.py
+++ app-st.py
@@ -8,7 +8,12 @@ import re
 import time
 import io
 
-st.title("Keyword Research Fuzzy Lookup & QA")
+st.title("Keyword Research Fuzzy Lookup & Quality Assurance Review")
+
+st.write("Utilise this document to complete the following:")
+st.write("👉 Find near duplicate keywords that have the same search volume (e.g. "shoes" and "shoe") - if they have the same search volume they're likely grouped and therefore keeping both will be inflating your data")
+st.write("👉 Misspellings - sometimes the smallest errors are the hardest - working out somehting is spelled wrong (see what I did there?)")
+st.write("👉 Special characters - this will highlight as a 'misspelling' if it sees a special character used")
 
 ignore_words = st.text_input("Add all words you want to ignore as part of the spell check, such as branded terms, product lines, etc.")
 sim_score = st.slider("What similarity score do you want to use for your dataset? The default is 96, however you can adapt as you see necessary between 90 and 100", min_value=90, max_value=100, value=96)
