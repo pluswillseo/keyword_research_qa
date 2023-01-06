b11d55e2ae31624bd7a393bfd61d45ea8289569c
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2023-01-06 10:04:22 +0000

    Update app-st.py

--- app-st.py
+++ app-st.py
@@ -219,6 +219,7 @@ if uploaded_file is not None:
     filtered_df = df[(df['Misspelling or special character'] == selected_categories) & (df["Duplicate with 's'"] == duplicate_s)]
     csv = filtered_df.to_csv(index=False)
     st.download_button('Download Table as CSV', csv, file_name = 'output.csv', mime='text/csv')
+    st.table(filtered_df)
     
 #    if selected_categories == []:
 #        csv = df.to_csv(index=False)
