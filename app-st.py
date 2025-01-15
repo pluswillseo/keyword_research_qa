7119315d8c74bd7db52c5ff1438f3bce6cf0b3bd
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2025-01-15 18:00:28 +0000

    Update app-st.py adding comma to output

--- app-st.py
+++ app-st.py
@@ -205,7 +205,7 @@ if uploaded_file is not None:
                     continue
 
                 if len(spell_checker.unknown([word])) > 0 or re.search(regex, word, re.UNICODE):
-                    df.loc[df['Keyword'] == keyword, 'Misspelling or special character'] += word
+                    df.loc[df['Keyword'] == keyword, 'Misspelling or special character'] += ", " + word
                     
         return df
     
